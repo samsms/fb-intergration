9cb85b680773ff6826e26fd98643cb00ed323393
root <samsaf674@gmail.com>
2022-03-31 05:15:46 +0300

    d

--- send.php
+++ send.php
@@ -17,10 +17,8 @@ $fb = new Facebook\Facebook([
 
 try {
 
-$data=[
-  "recipient": "{id: $recipient}",
-  "message": "{text:$message}"
-];
+$data='{"recipient": "{id: $recipient}","message": "{text:$message}"}';
+echo $data;
 $messages=$fb->post("/kkaalliance/messages}",$data);
 
 
