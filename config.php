8c2ff792e4c56c77cae934292d20488092b1a957
root <samsaf674@gmail.com>
2022-03-31 20:55:50 +0300

    dd

--- config.php
+++ config.php
@@ -13,8 +13,8 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.10',
   'default_access_token'=>$_SESSION['facebook_page_access_token']
   ]);
-function pageid(){
-    return $fb->get("/$page");
+function pageid($fb){
+    return $fb->get("/$page")->getDecodedBody();
 }
 
 ?>
