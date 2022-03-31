c4e3b7a52c5a942a5c32e359e140ecad363fcdd9
root <samsaf674@gmail.com>
2022-04-01 02:17:03 +0300

    dd

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 $page_id="467734418431236";
 $app_secret="79f8c3442e533b930a0ca9a9084d5019";
@@ -13,6 +12,8 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.10',
   'default_access_token'=>$_SESSION['facebook_page_access_token']
   ]);
-
+function pageid($fb){
+    return $fb->get("/$page")->getDecodedBody();
+}
 
 ?>
--- read.php
+++ read.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 header("content-type:application/json");
 require('config.php');
 if(!isset($_GET['thread_id'])){
