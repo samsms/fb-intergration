1dd4e5d37ef86debdb20009f30e4ce3e6f7d5aad
root <samsaf674@gmail.com>
2022-04-01 02:19:48 +0300

    dd

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['facebook_page_access_token'])){
-
+require("config.php");
 $helper = $fb->getRedirectLoginHelper();
 $loginUrl = $helper->getLoginUrl('https://sam-fb.herokuapp.com/webhook.php');
 header("location:$loginUrl"); 
