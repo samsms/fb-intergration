f78f2935915285714c55cd2966a05ff793e16677
root <samsaf674@gmail.com>
2022-03-31 20:39:21 +0300

    dd

--- api-chat.php
+++ api-chat.php
@@ -1,34 +1,18 @@
 <?php
 header("content-type:application/json");
-session_start();
- ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
- require_once __DIR__ . '/vendor/autoload.php';
-$fb = new Facebook\Facebook([
-  'app_id' => '467734418431236',
-  'app_secret' => '79f8c3442e533b930a0ca9a9084d5019',
-  'default_graph_version' => 'v2.10',
-  'default_access_token'=>$_SESSION['facebook_page_access_token']
-  ]);
 $path="";
 if(isset($_GET['path'])){
-
   $path="/".$_GET['path'];
 }
-
 try {
 
 $field=$_GET["field"];
-$messages=$fb->get("/kkaalliance/conversations$path?fields=$field");
-
-
+$messages=$fb->get("/$page/conversations$path?fields=$field");
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
+  
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
 }
---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__ . '/vendor/autoload.php';
+$page_id="467734418431236";
+$app_secret="79f8c3442e533b930a0ca9a9084d5019";
+$page="kkaalliance";
+$fb = new Facebook\Facebook([
+  'app_id' => $page_id,
+  'app_secret' => $app_secret,
+  'default_graph_version' => 'v2.10',
+  'default_access_token'=>$_SESSION['facebook_page_access_token']
+  ]);
+function pageid(){
+    return $fb->get("/$page");
+}
+
+?>
