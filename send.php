e47370fec82ba44cd991d10a8d1b534fdccfa453
sam <samsaf674@gmail.com>
2022-04-13 17:31:08 +0300

    dd

--- send.php
+++ send.php
@@ -2,19 +2,6 @@
 require('config.php');
 $recipient=$_POST['recipient'];
 $message=$_POST['msg'];
-try {
- $id= $_GET['thread_id'];
-$messages=$fb->get("$id?fields=messages{message,created_time,from,to}");
-
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-    echo json_encode("error");
- 
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-    echo json_encode("error");
-
-}
 
 try {
 
@@ -36,6 +23,21 @@ $messages=$fb->post("/$id/messages",$data);
  
    echo json_encode("error");
 }
+try {
+ $id= $_GET['thread_id'];
+$messages=$fb->get("$id?fields=messages{message,created_time,from,to}");
+  echo json_encode( $messages->getDecodedBody());
 
-echo json_encode( $messages->getDecodedBody());
-);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+    echo json_encode("error");
+ 
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+    echo json_encode("error");
+
+}
+
+
+
+?>
