6f4fbf1622c578aedb3f00d6d1c555f88db79a27
sam <samsaf674@gmail.com>
2022-04-13 16:50:15 +0300

    dd

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require("config.php");
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript"> 
-
+var id;
 $(document).ready(function(){
     $('#action_menu_btn').click(function(){
         $('.action_menu').toggle();
@@ -34,6 +34,7 @@ $(document).ready(function(){
             recipient:number,
             msg:message
         },function(res){
+            fetchChats(window.id);
 
         });
    })
@@ -64,7 +65,7 @@ response.data.forEach(function(res){
 
 });
 function fetchChats(id){
-
+window.id=id;
   //  $("#"+id).css("background-color: rgba(0,0,0,0.3)");
    // var threads=[];
    //var chats=[];
